32211474ce675c0c7be1327d1ad8ef955e376bbb
andrewfstratton <A.Stratton@shef.ac.uk>
2020-07-15 21:15:33 +0100

    Add initial websocket handling

--- quando/src/main/java/uk/co/strattonenglish/quando/LocalServer.java
+++ quando/src/main/java/uk/co/strattonenglish/quando/LocalServer.java
@@ -1,10 +1,24 @@
 package uk.co.strattonenglish.quando;
 
+import javax.servlet.ServletException;
+
+import org.eclipse.jetty.http.pathmap.ServletPathSpec;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.server.WebSocketUpgradeFilter;
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
+import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
 
 public class LocalServer {
 	private static final int SERVER_PORT = 8080;
+
+	public static class LocalServerSocketCreator implements WebSocketCreator {
+		@Override
+		public Object createWebSocket(ServletUpgradeRequest req, ServletUpgradeResponse resp) {
+			return new LocalWebSocketHandler();
+		}
+	}
 
 	private static Server createServer(int port) {
 		Server server = new Server(port);
@@ -12,6 +26,20 @@ public class LocalServer {
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 		context.setContextPath("/");
 		server.setHandler(context);
+
+		// Add websocket as a filter
+		WebSocketUpgradeFilter websocketfilter;
+		try {
+			websocketfilter = WebSocketUpgradeFilter.configure(context);
+		// wsfilter.getFactory().getPolicy().setIdleTimeout(5000);
+		websocketfilter.addMapping(new ServletPathSpec("/ws/"), new LocalServerSocketCreator());
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Add time servlet
+		// context.addServlet(WebSocketServlet.class,"/ws/*");
 
 		context.addServlet(LocalhostHandler.class,"/*");
 		return server;
