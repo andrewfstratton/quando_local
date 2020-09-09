5a230d4accd5b89b1f274005f3109927de1e96d5
andrewfstratton <A.Stratton@shef.ac.uk>
2020-09-09 15:02:44 +0100

    Add broadcast socket message

--- quando/src/main/java/uk/co/strattonenglish/quando/WebSocketHandler.java
+++ quando/src/main/java/uk/co/strattonenglish/quando/WebSocketHandler.java
@@ -1,6 +1,9 @@
 package uk.co.strattonenglish.quando;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
@@ -9,44 +12,55 @@ import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
 @WebSocket
 public class WebSocketHandler implements Runnable {
-    private Session session;
-    int count = 0;
+  private Session session;
+  private static List<Session> sessions = new ArrayList<Session>();
+  int count = 0;
+  private static Thread thread = null;
 
-    @OnWebSocketMessage
-    public void onMessage(Session session, String message)
-    {
-      System.out.println(message + ":" + count);
+
+  @OnWebSocketMessage
+  public void onMessage(Session session, String message) {
+    System.out.println(message + ":" + count);
+  }
+
+  @OnWebSocketConnect
+  public void onOpen(Session session) {
+    System.out.println("Open:" + count);
+    this.session = session;
+    sessions.add(session);
+    if (thread == null) {
+      thread = new Thread(this);
+      thread.start();
     }
+  }
 
-    @OnWebSocketConnect
-    public void onOpen(Session session)
-    {
-      System.out.println("Open:" + count);
-        this.session = session;
-        new Thread(this).start();
-    }
+  @OnWebSocketClose
+  public void onClose(int closeCode, String closeReasonPhrase) {
+    System.out.println("Close:" + count);
+    sessions.remove(this.session);
+    this.session = null;
+  }
 
-    @OnWebSocketClose
-    public void onClose(int closeCode, String closeReasonPhrase)
-    {
-      System.out.println("Close:" + count);
-        this.session = null;
-    }
-
-    @Override
-    public void run()
-    {
-      while (this.session != null)
-      {
-        try {
-          this.session.getRemote().sendString(String.valueOf(++count));
-          Thread.sleep(1000);
-        } catch (IOException e) {
-          e.printStackTrace();
-        } catch (InterruptedException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
+  public static synchronized void broadcast(String msg) {
+    // Note: synchronized is essential here to avoid runtime error
+    for (Session session : sessions) {
+      try {
+        session.getRemote().sendString(msg);
+      } catch (IOException e) {
+        e.printStackTrace();
       }
     }
+  }
+
+  @Override
+  public void run() {
+    while (true) {
+      try {
+        broadcast(String.valueOf(sessions.size()) + ":" + String.valueOf(++count));
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 }
